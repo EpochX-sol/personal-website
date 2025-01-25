4c64417a4fafeea756e2504527f292c42c67c933
Epochx <85428078+EpochX-sol@users.noreply.github.com>
2025-01-26 00:37:41 +0300

    update experiance

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -103,7 +103,7 @@ export default function About() {
                     </li>
                     <li className="flex items-start">
                       <CheckCircle2 className="w-5 h-5 mr-2 text-blue-500 shrink-0 mt-0.5" />
-                      <span>Implement backend by using .NET</span>
+                      <span>Devleop UIs using React and backend solutions using .NET and Python</span>
                     </li>
                   </ul>
                 </div>
